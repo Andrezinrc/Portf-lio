5a3fb966cb4a432f51923c1d727298c749c3f69c
Andrezinrc <andrem.33333@gmail.com>
2025-07-28 03:45:02 +0000

    feat: Adiciona seção sobre mim

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from './sections/Hero';
-
+import About from './sections/About';
+  
 function Content(){
   return (
     <main className="content">
       {/* Seção Hero: destaque inicial com título, parágrafo e links sociais */}
 
       <Hero />
+
+      {/* Seção: Sobre mim */}
+      <About />
     </main>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,49 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Menu() {
-  const [menuOpen, setIsMenuOpen] = useState(false);
-
-  // Função para alternar o estado do menu
-  const toggleMenu = () => {
-    setIsMenuOpen(!menuOpen);
-  }
-
-  function handleLinkClick() {
-    setIsMenuOpen(false); // Fecha o menu ao clicar no link
-  }
-
-
+  
   return (
     <nav className="nav-bar">
       {/* Menu */}
       <ul className="menu">
         <li className="nav-list">
-          <a href="#" className="nav-link" onClick={handleLinkClick}>
+          <a href="#" className="nav-link">
             <i className="fas fa-home"></i>
           </a>
         </li>
         <li className="nav-list">
-          <a href="#about" className="nav-link" onClick={handleLinkClick}>
+          <a href="#about" className="nav-link">
             <i className="fas fa-user"></i>
           </a>
         </li>
         <li className="nav-list">
-          <a href="#skills" className="nav-link" onClick={handleLinkClick}>
+          <a href="#skills" className="nav-link">
             <i className="fas fa-tools"></i>
           </a>
         </li>
         <li className="nav-list">
-          <a href="#certificates" className="nav-link" onClick={handleLinkClick}>
+          <a href="#certificates" className="nav-link">
             <i className="fas fa-certificate"></i>
           </a>
         </li>
         <li className="nav-list">
-          <a href="#projects" className="nav-link" onClick={handleLinkClick}>
+          <a href="#projects" className="nav-link">
             <i className="fas fa-code"></i>
           </a>
         </li>
         <li className="nav-list">
-          <a href="#contact" className="nav-link" onClick={handleLinkClick}>
+          <a href="#contact" className="nav-link">
             <i className="fas fa-envelope"></i>
           </a>
         </li>
